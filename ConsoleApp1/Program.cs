f8ba6fbf9bb548b41d6a701f52baf99a372b5c9a
Halkman1987 <Halk-man@mail.ru>
2021-11-28 20:09:10 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System;
+
+class Mainclass
+{
+    public static void Main(string[] args)
+    {
+        string MyName;
+        MyName = "Dima";
+        Console.WriteLine(MyName);
+
+        Console.ReadKey();
+    }
+
+}
+
