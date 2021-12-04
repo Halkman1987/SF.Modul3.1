9f703da72f78f966a77b6a5789688cb34ac756e9
Halkman1987 <Halk-man@mail.ru>
2021-12-04 15:32:42 +0500

    Задания в модуле 3

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,54 +8,120 @@ class Mainclass
     /// <param name="args"></param>
     public static void Main(string[] args)
     {
-
-        /*const string MyName = "Dima\n";
+        {
+            /*const string MyName = "Dima\n";
         Console.WriteLine(MyName);
         Console.WriteLine("\t\u0023");
-        Console.WriteLine("Ну вот \t\tкак-то так");
+        Console.WriteLine("Ну вот \t\t как-то так");
         Console.ReadKey();*/
 
-        /* Console.Write("Напиши возраст:");
-         int age = Convert.ToInt32(Console.ReadLine());
-         Console.WriteLine("Ваш возраст {0}", age);
+            /* Console.Write("Напиши возраст:");
+             int age = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("Ваш возраст {0}", age);
+             Console.ReadKey();*/
+
+        }
+
+
+
+    
+
+
+        /* Console.WriteLine("Введите свое имя");
+         var name = Console.ReadLine();
+         Console.WriteLine("Введите свой возраст");
+         var age = checked((byte)int.Parse(Console.ReadLine()));
+         Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+
+         Console.Write("What is your favorite day of week? ");
+
+         var day = (DayOfWeek)int.Parse(Console.ReadLine());
+         Console.WriteLine("Your favorite day is {0}", day);
          Console.ReadKey();*/
 
-        /*Console.Write("Ваше имя ");
-        string name = Console.ReadLine();
-        Console.Write("Ваш возраст ");
-        int age = int.Parse(Console.ReadLine());
-        Console.WriteLine("Ваше имя {0} и ваш возраст {1} ", name, age);
-        Console.ReadKey();
-*/ /*
-        Console.Write("Enter your name: ");
-        string name = Console.ReadLine();
-        Console.Write("Enter your age: ");
-        byte age = checked((byte)int.Parse(Console.ReadLine()));
-        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
-        Console.Write("какой твой любимый день недели? ");
-        DayOfWeek day = (DayOfWeek)int.Parse(Console.ReadLine());
-        Console.WriteLine("Твой любимый день {0} ", day);
-        Console.ReadKey();
-
-        var age = checked((byte)int.Parse(Console.ReadLine()));
-        Console.WriteLine("Ваше имя {0} и возраст {1}", name, age);
-        Console.WriteLine("Ваш любимый день недели?");
-        var day = (DayOfWeek)int.Parse(Console.ReadLine());
-        Console.WriteLine("Твой любимый день {0} ", day);
-        Console.ReadKey();*/
 
 
-        Console.WriteLine("Введите свое имя");
-        var name = Console.ReadLine();
-        Console.WriteLine("Введите свой возраст");
-        var age = checked((byte)int.Parse(Console.ReadLine()));
-        Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+        // Инкремент и декремент 
+        {
+            /* int counter = 10;
+             Console.WriteLine("Value:{0} decrement:{1}", counter, --counter);
+             Console.ReadKey();*/
+        }
 
-        Console.Write("What is your favorite day of week? ");
 
-        var day = (DayOfWeek)int.Parse(Console.ReadLine());
-        Console.WriteLine("Your favorite day is {0}", day);
-        Console.ReadKey();
+
+        //конвертирование строки в числовое значение
+        {
+            /*Console.Write("Укажите свой возраст:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ваш Возраст {0}", age);
+            Console.ReadKey();*/
+        }
+
+        //Конвертирование через Parse и TryParse 
+        {
+            /*Console.Write("Укажите свой возраст:");
+            int age = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ваш Возраст {0}", age);
+            Console.ReadKey();*/
+
+        }
+
+        //Через TryParse и bool значение
+        {
+           /* Console.Write("Укажите свой возраст:");
+            int age;
+            bool iscorrect = int.TryParse(Console.ReadLine(), out age);
+            Console.WriteLine("Ваш Возраст {0}", age);
+            Console.ReadKey();*/
+        }
+
+        //Продолжаем развлекаться...вводим возраст больше byte. Но если указать
+        //   int intage = age;
+        //   Console.WriteLine("Your name is {0} and age is {1} ", name, intage); произойдет процесс расширения переменной age в intage
+        {
+           /* Console.Write("Ваше имя ");
+            string name = Console.ReadLine();
+            Console.Write("Ваш возраст : ");
+            byte age = (byte)int.Parse(Console.ReadLine());
+            Console.WriteLine("Ваше имя {0} и ваш возраст {1} ", name, age);
+            Console.ReadKey();*/
+
+        }
+        // Проверка конвертации посредством checked
+        {
+            /*Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte age = checked((byte)int.Parse(Console.ReadLine()));
+            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+            Console.Write("какой твой любимый день недели?: ");
+            DayOfWeek day = (DayOfWeek)int.Parse(Console.ReadLine());
+            Console.WriteLine("Твой любимый день {0} ", day);
+            Console.ReadKey();*/
+        }
+
+        //Объявление неявной типизации через переменную var, она сама определяет тип по присвоенному значению переменной
+        {
+            Console.Write("Введите свое имя:");
+            var name = Console.ReadLine();
+            Console.Write("Введите свой возраст:");
+            var age = checked((byte)int.Parse(Console.ReadLine()));
+            Console.WriteLine("Ваше имя {0} и возраст {1}", name, age);
+            Console.WriteLine("Ваш любимый день недели?");
+            var day = (DayOfWeek)int.Parse(Console.ReadLine());
+            Console.WriteLine("Твой любимый день {0} ", day);
+            Console.ReadKey();
+        }
+        /*
+
+                //Семафор перечисления
+                enum Semaphore
+                {
+                Red = 100,
+                Yellow = 200,
+                Green = 300
+                }*/
 
     }
 
