3a5ee1ee88ac986ed1ab79536f595d123a8b5a57
Halkman1987 <Halk-man@mail.ru>
2021-12-04 21:00:52 +0500

    mod4

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -63,9 +63,42 @@ class Mainclass
 
         
 
+        }
+        // Проверка конвертации посредством checked
+        {
+            /*Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            byte age = checked((byte)int.Parse(Console.ReadLine()));
+            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
+            Console.Write("какой твой любимый день недели?: ");
+            DayOfWeek day = (DayOfWeek)int.Parse(Console.ReadLine());
+            Console.WriteLine("Твой любимый день {0} ", day);
+            Console.ReadKey();*/
+        }
+
+        //Объявление неявной типизации через переменную var, она сама определяет тип по присвоенному значению переменной
+        {
+            Console.Write("Введите свое имя:");
+            var name = Console.ReadLine();
+            Console.Write("Введите свой возраст:");
+            var age = checked((byte)int.Parse(Console.ReadLine()));
+            Console.WriteLine("Ваше имя {0} и возраст {1}", name, age);
+            Console.WriteLine("Ваш любимый день недели?");
+            var day = (DayOfWeek)int.Parse(Console.ReadLine());
+            Console.WriteLine("Твой любимый день {0} ", day);
+            Console.ReadKey();
+        }
         
-            
-        
-    
-    
+
+                //Семафор перечисления
+                enum Semaphore
+                {
+                Red = 100,
+                Yellow = 200,
+                Green = 300
+                }
+
+    }
+
 }
