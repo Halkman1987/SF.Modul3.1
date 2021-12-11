ec562206aeac78b3431af2f83772e9af5322644f
Halkman1987 <Halk-man@mail.ru>
2021-12-11 19:35:15 +0500

    Домашяя анкета модуль 4 финальное задание

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,76 +1,78 @@
-﻿
-using System;
+﻿using System;
 
 class Mainclass
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("как вас зовут? ");
-        string Imya = Console.ReadLine();
-        Console.WriteLine("Сколько вам лет? ");
-        var age = int.Parse(Console.ReadLine());
-        bool pet = true;
-        Console.WriteLine("У вас есть питомец? "+ pet);
-        double leg = 44.5;
-        Console.WriteLine("У вас размер ноги " + leg);
-        Console.WriteLine("Вас зовут {0} вам {1} лет , также у вас {2} питомец и у вас размер ноги {3}", Imya, age, pet, leg);
-        Console.ReadKey();
+                (string name, string family, string login, int loginlength, bool pet, double age, string[] color) User;
+
+        for (int j = 0; j < 3; j++)
+        {
+            Console.WriteLine("Введите имя пользователя : ");
+            User.name = Console.ReadLine();
+
+            Console.WriteLine("Введите фамилию пользователя : ");
+            User.family = Console.ReadLine();
+
+            Console.WriteLine("Введите логин пользователя : ");
+            User.login = Console.ReadLine();
+            User.loginlength = User.login.Length;
+            Console.WriteLine("Длинна логин пользователя : {0}", User.loginlength);
+            Console.WriteLine("Имеется ли у вас питомец Да/Нет : ");
+            var result = Console.ReadLine();
+            if (result == "да")
+            {
+                User.pet = true;
+            }
+            else
+            {
+                User.pet = false;
+            }
+            Console.WriteLine("Введите ваш возраст : ");
+            User.age = double.Parse(Console.ReadLine());
+            User.color = new string[3];
+            Console.WriteLine("Введите три любимых цвета :");
+            for (int i = 0; i < User.color.Length; i++)
+            {
+                User.color[i] = Console.ReadLine();
+            }
+            Console.WriteLine();
+        }
+                Console.ReadKey();
+
     }
 }
-    
-    /*const string MyName = "Dima\n";
-    Console.WriteLine(MyName);
-         Console.WriteLine("\t\u0023");
-         Console.WriteLine("Ну вот \t\tкак-то так");
-         Console.ReadKey();
 
-        // Способ узнать вилку значений  числового типа данных
-        Console.WriteLine("Минимум {0} ", float.MinValue);
-        Console.WriteLine("Максимум {0} ", float.MaxValue);
-        Console.ReadKey();
-    
+// ----------------------------Анкета  о Питомце ---------------------------
 
+//        (string Name, string Type, double Age, int NameCount) Pet;
+//        Console.WriteLine("Введите имя питомца :");
+//        Pet.Name = Console.ReadLine();
+//        Console.WriteLine("Введите тип питомца :");
+//        Pet.Type = Console.ReadLine();
+//        Console.WriteLine("Введите возраст питомца :");
+//        Pet.Age = Double.Parse( Console.ReadLine());
+//        Pet.NameCount = Pet.Name.Length;
+//        Console.WriteLine("Длина имени питомца : {0} ", Pet.NameCount);
+//------------------------------------------------------------------------------------------------
+//var (name, age) = ("Dima", 33);
+//Console.WriteLine("You name :{0}", name);
+//Console.WriteLine("You age : {0}", age);
 
+//Console.Write("Insert you name:");
+//name = Console.ReadLine();
+//Console.Write("Insert you age :");
+//age = Convert.ToInt32(Console.ReadLine());
 
-
-
-    // Обозначение перечисления через оператор enum
-   
-    {
-        DaysOfWeeks MyFavoritedDay;
-    MyFavoritedDay = DaysOfWeeks.Sunday;
-        Console.WriteLine(MyFavoritedDay);
-    }
-}
-    enum DaysOfWeeks : byte
-{
-    Monday = 1,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday
-}
+//Console.WriteLine("You name: {0} and  age :{1}", name, age);
 
 
-// const byte Monday = 1;
-//const byte Tuesday = 2;
-//const byte Wednesday = 3;
-//const byte Thursday = 4;
-//const byte Friday = 5;
-//const byte Saturday = 6;
-//const byte Sunday = 7;
 
-enum ProgramData
-{
-    Programld = 123,
-    Authorld = 567
-}
+//(string name, int age) anketa;
+//Console.WriteLine("Ваше имя :");
+//anketa.name = Console.ReadLine();
+//Console.WriteLine("Введите ваш возраст цифрами :");
+//anketa.age = Convert.ToInt32(Console.ReadLine());
 
-enum Semaphore
-{
-    Reg = 100,
-    Yellow = 200,
-    Green = 300
-}*/
+//Console.WriteLine("Ваше имя :{0} ", anketa.name);
+//Console.WriteLine("Ваш возраст :{0} ", anketa.age);
