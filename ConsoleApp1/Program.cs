6689b20022368a4b4eaacc59efc982ca83907b68
Halkman1987 <Halk-man@mail.ru>
2021-12-12 16:18:49 +0500

    1

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,84 +1,14 @@
 ﻿using System;
 
 class MainClass
-{/*
-    static string GetDataFromConsole()
+   {
+    static void Main(string[] args);
     {
-        return Console.ReadLine();
+     GetArrayFromConsole();
     }
-    static string ShowColor()
-    {
-        var favcolors = new string[3]; //объявляем массив из трех элементов
-        //Console.WriteLine("Напишите цвета :");
-        
-        for (int i = 0; i < favcolors.Length; i++)
-        {
-            
-           favcolors[i] = ShowColor();
-
-        }
-        Console.WriteLine("Ваши любимые цвета :");
-        foreach (var color in favcolors)
-        {
-            Console.WriteLine(color);
-
-            switch (color)
-            {
-                case "red":
-                    Console.BackgroundColor = ConsoleColor.Red;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is red!");
-                    break;
-
-                case "green":
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is green!");
-                    break;
-                case "cyan":
-                    Console.BackgroundColor = ConsoleColor.Cyan;
-                    Console.ForegroundColor = ConsoleColor.Black;
-
-                    Console.WriteLine("Your color is cyan!");
-                    break;
-                default:
-                    Console.BackgroundColor = ConsoleColor.Yellow;
-                    Console.ForegroundColor = ConsoleColor.Red;
-
-                    Console.WriteLine("Your color is yellow!");
-                    break;
-
-            }
-           // return color;
-        }
-        
-        Console.ReadKey();
-    }
-    public static void Main(string[] args)
-    {
-
-        var (name, age) = ("Евгения", 27);
-
-        Console.WriteLine("Мое имя: {0}", name);
-        Console.WriteLine("Мой возраст: {0}", age);
-
-        Console.Write("Введите имя: ");
-        name = Console.ReadLine();
-        Console.Write("Введите возрас с цифрами:");
-        age = Convert.ToInt32(Console.ReadLine());
-
-        Console.WriteLine("Ваше имя: {0}", name);
-        Console.WriteLine("Ваш возраст: {0}", age);
-
-        ShowColor();
 
 
-    }
-*/
-
-    static int[] GetArrayFromConsole()
+       static int[] GetArrayFromConsole();
     {
         var result = new int[5];
 
@@ -107,7 +37,7 @@ class MainClass
         {
             Console.Write(res);
         }
-        return result;
+        
     }
-
+}
 }
